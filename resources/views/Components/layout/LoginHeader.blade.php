d754f61e1a434da29246268c44c5a60dff8bf22c
ArthurDeAraujoNeves3 <arthurdearaujoneves333@gmail.com>
2024-09-12 16:21:56 -0300

    styles: Criado o visual da página de Login

---
+++ resources/views/Components/layout/LoginHeader.blade.php
@@ -0,0 +1,9 @@
+<header>
+
+    <figure>
+
+        <img class="Logo" src={{ URL::asset("src/assets/img/Logo/LogoTheme.png") }} alt="Admin" draggable="false" loading="lazy" />
+
+    </figure>
+
+</header>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,16 +1,48 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
 
-    <link rel="stylesheet" href="{{ URL::asset("src/index.css") }}"> <!--Index-->
+    <link rel="stylesheet" href="{{ URL::asset('src/bootstrap/css/bootstrap.min.css') }}"> <!--Bootstrap-->
+    <link rel="stylesheet" href="{{ URL::asset('src/index.css') }}"> <!--Index-->
+    <link rel="stylesheet" href="{{ URL::asset('src/styles/Pages/Login/Loginstyle.css') }}"> <!--Login-->
+    <link rel="stylesheet" href="{{ URL::asset('src/styles/Pages/Login/LoginHeaderstyles.css') }}">
+
 </head>
+
 <body>
 
-    <p>Login</p>
-    
+    <main class="">
+
+        <form class="Form" method="post">
+            
+            @component("Components.layout.LoginHeader")
+            @endcomponent
+            
+            <div class="mb-3">
+
+                <label for="exampleInputEmail1" class="form-label">Email</label>
+                <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp">
+                
+            </div> {{--Email--}}
+
+            <div class="mb-3">
+
+                <label for="exampleInputPassword1" class="form-label">Senha</label>
+                <input type="password" class="form-control" name="password" id="exampleInputPassword1">
+
+            </div> {{--Senha--}}
+
+            <button type="submit" class="btn btn-primary">Login</button>
+
+        </form>
+
+    </main>
+
 </body>
+
 </html>
